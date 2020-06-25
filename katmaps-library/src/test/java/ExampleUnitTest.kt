809d5d39b378d8b80f763f5a46f1ce68a4421ca9
chjanusa <chjanusa@groupon.com>
2020-06-25 11:24:01 -0700

    TEST

---
+++ katmaps-library/src/test/java/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun false_ReturnsTrue() {
+        assert(false)
+    }
+
+    @Test
+    fun true_ReturnsTrue() {
+        assert(true)
+    }
+}
